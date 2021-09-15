8c6f7803814e9d03ccf43252558a076072ce8004
Geraldo Moura <geraldo.moura@ymail.com>
2021-09-15 15:26:21 -0500

    using async function to search data before posting

--- js/app.js
+++ js/app.js
@@ -1,8 +1,38 @@
-const Http = new XMLHttpRequest();
-const url = 'https://swapi.dev/api/starships/';
-Http.open("GET", url);
-Http.send();
+// const Http = new XMLHttpRequest();
+// const url = 'https://swapi.dev/api/starships/';
+// Http.open("GET", url);
+// Http.send();
 
-Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
+// Http.onreadystatechange = (e) => {
+//     const data = Http.responseText
+//     console.log(data)
+// }
+
+const results = document.querySelector('#results');
+
+let text = 'r2';
+
+async function fetchData(value) {
+    // fetch('https://swapi.dev/api/starships/')
+    //     .then(response => response.json())
+    //     .then(data => console.log(data));
+    const res = await fetch(`https://swapi.dev/api/people/?search=${value}`);
+    const data = await res.json();
+    console.log(data)
 }
+
+fetchData(text);
+
+// fetchData();
+
+// Event listener to seach for spaceship
+// document.querySelector('#search').addEventListener('click', () => {
+//     // When clicked will fetch all starships from API
+//     asyncFetch('starships');
+//     // Getting the value from input
+//     // const searchWord = document.querySelector('input').value;
+
+//     // console.log(data);
+// })
+
+
