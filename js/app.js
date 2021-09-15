cc832cedc6425a7d7d629be5f446b2d20c7e47ff
Geraldo Moura <geraldo.moura@ymail.com>
2021-09-15 13:52:30 -0500

    sent it

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('Test!!!')
+const Http = new XMLHttpRequest();
+const url = 'https://swapi.dev/api/starships/';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+    console.log(Http.responseText)
+}
