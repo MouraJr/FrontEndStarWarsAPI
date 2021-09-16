e50cf3ddca4addfe6f70698d3cc68e44ac14badd
Geraldo Moura <geraldo.moura@ymail.com>
2021-09-16 15:46:23 -0500

    saving changes before big change

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 // https://geraldomoura-firstfrontend-swapi.surge.sh/
 
 // Using async function to get data and turn into Json
-async function fetchData(value) {
+async function fetchData(url) {
     // Await is making sure the promise is loaded before moving to next line
-    const res = await fetch(`https://swapi.dev/api/starships/?search=${value}`);
+    const res = await fetch(url);
     const data = await res.json();
-    displayResults(data, value);
-    console.log(data)
+    console.log(data);
+    // displayResults(data);
 }
 
-function displayResults(data, value) {
+function displayResults(data) {
     const results = document.querySelector('#results');
     let output = '';
 
@@ -28,7 +28,7 @@ function displayResults(data, value) {
 
     data.results.forEach(element => {
         output += `
-            <div class=".col-lg-4 col-md-6 col-sm-12 col-12">
+            <div class="col-12 col-sm-12 col-md-6 col-lg-4">
                 <div class='card p-4 m-3'>
                     <h4 class='card-header'>${element.name}</h4>
                     <div class='card-body text-start'>
@@ -51,19 +51,46 @@ function displayResults(data, value) {
 }
 
 // Event listener to seach for spaceship
-document.querySelector('#search').addEventListener('click', () => {
+const btnSearch = (btnToSearch, inputToSearch) => document.querySelector(btnToSearch).addEventListener('click', () => {
     // When clicked will fetch all starships from API
     // Get the word from input field
-    const searchWord = document.querySelector('input').value;
-    fetchData(searchWord);
+    const searchWord = document.querySelector(inputToSearch).value;
+    // Declare variable for url
+    let url = '';
+    if (inputToSearch === '#inputSearchStarships') {
+        url = `https://swapi.dev/api/starships/?search=${searchWord}`;
+    };
+
+    if (inputToSearch === '#inputSearchCharacters') {
+        url = `https://swapi.dev/api/people/?search=${searchWord}`;
+    };
+
+    if (inputToSearch === '#inputSearchPlanets') {
+        url = `https://swapi.dev/api/planets/?search=${searchWord}`;
+    };
+
+    // Invoke data with fetch function
+    fetchData(url);
 });
 
-document.querySelector('#inputSearch').addEventListener('keydown', function (e) {
+const inputSearch = (btnToSearch, inputToSearch) => document.querySelector(btnToSearch).addEventListener('keydown', function (e) {
     if (e.keyCode === 13) {
-        // When clicked will fetch all starships from API
+        // When enter will fetch all starships from API
         // Get the word from input field
-        console.log('works')
-        const searchWord = document.querySelector('input').value;
-        fetchData(searchWord);
+        const searchWord = document.querySelector(inputToSearch).value;
+        const url = `https://swapi.dev/api/starships/?search=${searchWord}`;
+        fetchData(url);
     }
 });
+
+// Firing the Searching Starships
+btnSearch('#searchBtnStarships', '#inputSearchStarships');
+inputSearch('#searchBtnStarships', '#inputSearchStarships');
+
+// Firing the Searching Characters
+btnSearch('#searchBtnCharacters', '#inputSearchCharacters');
+inputSearch('#searchBtnCharacters', '#inputSearchCharacters');
+
+// Firing the Searching Planets
+btnSearch('#searchBtnPlanets', '#inputSearchPlanets');
+inputSearch('#searchBtnPlanets', '#inputSearchPlanets');
